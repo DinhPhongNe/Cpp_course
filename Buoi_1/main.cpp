b0ed45f6320f31e3d32f461da5f40a1f7db545cc
DinhPhongNe <phongvo2405@example.com>
2024-01-09 21:27:36 +0700

    first commit

---
+++ Buoi_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Khai báo biến để lưu trữ số tuổi
+    int tuoi;
+
+    // Nhập vào số tuổi của bạn Duy
+    std::cout << "Nhap vao so tuoi cua ban Duy: ";
+    std::cin >> tuoi;
+
+    // In ra giá trị vừa nhập
+    std::cout << "Tuoi cua Duy la " << tuoi << std::endl;
+
+    return 0;
+}
